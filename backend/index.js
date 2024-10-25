2cce6cd4a2aa59132e168d7c5dba081fd92edbf7
Indhiran-god <indhiran736@gmail.com>
2024-10-25 07:16:28 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -8,18 +8,32 @@ const router = require('./routes');
 const app = express();
 
 // Middleware
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {  // Allow requests with no origin (like mobile apps or curl)
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true  // Allow credentials like cookies
 }));
+
+// You don't need app.options('*', cors()); if CORS is correctly configured
+// This already handles preflight OPTIONS requests based on the CORS configuration above.
+
 app.use(cookieParser());
 app.use(express.json());
 
 // Set a longer timeout for requests
 app.use((req, res, next) => {
-    res.setTimeout(30000, () => { // 30 seconds
+    res.setTimeout(30000, () => {  // 30 seconds
         console.log('Request has timed out.');
-        res.sendStatus(408); // Send a 408 Request Timeout status code
+        res.sendStatus(408);  // Send a 408 Request Timeout status code
     });
     next();
 });
