18d8f12c546b53fd56471a056d5f667034eafdc3
Indhiran-god <110456933+Indhiran-god@users.noreply.github.com>
2024-10-26 06:12:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ const router = require('./routes');
 const app = express();
 
 // Define allowed origins
-const allowedOrigins = ['https://administrator-a9im.vercel.app/'];
+const allowedOrigins = ['https://administrator-a9im-6h1cri9sd-indhiran-gods-projects.vercel.app',
+    'https://administrator-a9im.vercel.app'];
 
 // CORS configuration
 app.use(cors({
